6ebdc9d77c92efbedbff7fcab92d46c8357cc930
Antroll <panov-rabota@mail.ru>
2016-05-17 11:25:09 +0300

    update

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,55 +1,52 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
-	// cut text overflow
-	// $(".dot").dotdotdot();
+    $(".iframe").colorbox({
+    	iframe: true,
+    	width: "95%",
+    	height: "95%",
+    	onOpen: addButtons
+    });
 
-	// check window witdh
-	// if ($(window).width() < 960) {}
+    var myTimeout = null;
+    $(window).resize(function() {
+    	clearTimeout(myTimeout);
+    	myTimeout = setTimeout(function() {
+    		Resize_Box();
+    	}, 100);
+    })
 
-	// $(".iframe").colorbox({
-	// 	iframe:true,
-	// 	width:"95%",
-	// 	height:"95%",
-	// 	onOpen: addButtons
-	// });
-	$(document).on('click', '.iframe', function (e) {
-		e.preventDefault();
-		var
-		$modal = $(''
-			+'<div class="my-modal my-modal_iframe">'
-			+'<div class="my-modal__close arcticmodal-close">закрыть</div>'
-			+'<iframe frameborder="0" name="" allowfullscreen="" src="" class="my-modal__iframe"></iframe>'
-			+'</div>'
-			)
-		,$url = $(this).attr('href')
-		;
-
-		$modal.arcticmodal({
-			beforeOpen: function(data, el) {
-				$modal.find('iframe').attr('src', $url);
-			},
-		});
-	});
-
-	// close on focus lost
-	$(document).click(function(e) {
-		var $trg = $(e.target);
-		if (!$trg.closest(".parent-element").length && !$trg.hasClass('trigger-class') || $trg.hasClass('close-btn')) {
-			$('.block').removeClass('active');
-			$('.spoiler').slideUp(200);
-		}
-	});
+    // close on focus lost
+    $(document).click(function(e) {
+    	var $trg = $(e.target);
+    	if (!$trg.closest(".parent-element").length && !$trg.hasClass('trigger-class') || $trg.hasClass('close-btn')) {
+    		$('.block').removeClass('active');
+    		$('.spoiler').slideUp(200);
+    	}
+    });
 
 });
 $(window).load(function() {
-	// preloader
-	// $('#status').fadeOut();
-	// $('#preloader').delay(350).fadeOut('slow');
-	// $('body').delay(350).css({
-	// 	'overflow': 'visible'
-	// });
+    // preloader
+    // $('#status').fadeOut();
+    // $('#preloader').delay(350).fadeOut('slow');
+    // $('body').delay(350).css({
+    // 	'overflow': 'visible'
+    // });
 });
 
 function addButtons() {
-	console.log('it work');
+	var
+	$t = $(this),
+	href = $t.attr('href'),
+	$btn = '<a class="cboxFull" href=' + href + ' target="_blank"></a>';
+	$('#cboxContent').append($btn)
+}
+
+function Resize_Box(){
+	var x = $('body').width() - 30;
+	var y = $('body').height() - 30;
+	parent.$.fn.colorbox.resize({
+		innerWidth: x,
+		innerHeight: y
+	});
 }
---
+++ dist/scripts/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+
+    $(".iframe").colorbox({
+        iframe: true,
+        width: "95%",
+        height: "95%",
+        onOpen: addButtons
+    });
+
+    var myTimeout = null;
+    $(window).resize(function () {
+        clearTimeout(myTimeout);
+        myTimeout = setTimeout(function () {
+            Resize_Box();
+        }, 100);
+    });
+
+    // close on focus lost
+    $(document).click(function (e) {
+        var $trg = $(e.target);
+        if (!$trg.closest(".parent-element").length && !$trg.hasClass('trigger-class') || $trg.hasClass('close-btn')) {
+            $('.block').removeClass('active');
+            $('.spoiler').slideUp(200);
+        }
+    });
+});
+$(window).load(function () {
+    // preloader
+    // $('#status').fadeOut();
+    // $('#preloader').delay(350).fadeOut('slow');
+    // $('body').delay(350).css({
+    // 	'overflow': 'visible'
+    // });
+});
+
+function addButtons() {
+    var $t = $(this),
+        href = $t.attr('href'),
+        $btn = '<a class="cboxFull" href=' + href + ' target="_blank"></a>';
+    $('#cboxContent').append($btn);
+}
+
+function Resize_Box() {
+    var x = $('body').width() - 30;
+    var y = $('body').height() - 30;
+    parent.$.fn.colorbox.resize({
+        innerWidth: x,
+        innerHeight: y
+    });
+}
+//# sourceMappingURL=main.js.map
